ae4d31707e691d303a4bc7f7d48dc46da11d4e84
= <=>
2026-05-25 14:18:24 +0800

    Initial Commit

---
+++ frontend/src/(public)/Welcome.tsx
@@ -0,0 +1,10 @@
+const WelcomePage = () => {
+
+    return(
+        <div>
+            <h1 className="text-4xl font-bold text-center mt-10">Welcome to Slideshow</h1>
+        </div>
+    )
+}
+
+export default WelcomePage;
---
+++ frontend/src/main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <div className="font-sans">
+        <App />
+      </div>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
